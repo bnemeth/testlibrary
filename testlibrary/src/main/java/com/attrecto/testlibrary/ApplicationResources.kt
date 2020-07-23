e0bed31cf4c4fd1b38010a4e713bc2108eff7b29
balazsnemeth <balazs.nemeth@attrecto.com>
2020-07-23 13:44:03 +0200

    Add * for repository test

--- testlibrary/src/main/java/com/attrecto/testlibrary/ApplicationResources.kt
+++ testlibrary/src/main/java/com/attrecto/testlibrary/ApplicationResources.kt
@@ -5,7 +5,7 @@ import android.content.Context
 interface ApplicationResources {
     val applicationContext: Context
 
-    fun getString(resId: Int) = applicationContext.getString(resId)
+    fun getString(resId: Int) = applicationContext.getString(resId) + "*"
 
-    fun getString(resId: Int, vararg formats : Any) = applicationContext.getString(resId, formats)
+    fun getString(resId: Int, vararg formats : Any) = applicationContext.getString(resId, formats) + "*"
 }
